e46a1ffe7e941a03e01c538627775d8c005834ad
Ilya Kulikov <ilya-art@mail.ru>
2017-03-23 18:17:34 +0200

    One more Thing… And release

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      todos: [],
+      todos: [ ],
     };
 
     this.handleStatusChange = this.handleStatusChange.bind(this);
